0557ac413693600f1954d457e70418a57fdd6c81
[mochangil] <ahckddlf1234@gmail.com>
2023-05-03 17:52:01 +0900

    Feat : Teacher CRUD

--- src/controllers/memberController.js
+++ src/controllers/memberController.js
@@ -1,10 +1,47 @@
 const models = require('../database/models');
 
-const getTeachers = async (ctx) =>{
-    const teachers = await models.Teacher.findAll();
-    ctx.body = teachers;
+const getTeachers = async (req,res,next) =>{
+    try{
+        const teachers = await models.Teacher.findAll();
+        res.send(teachers);
+    }
+    catch (err){
+        next(err);
+    }
 };
 
+const createTeacher = async (req,res,next) =>{
+    try{
+        console.log("connected")
+        const counts = await models.Teacher.max('id');
+        const newTeacher = models.Teacher.build({name: req.params.name});
+        newTeacher.set({
+            id: counts+1,
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            age: req.body.age,
+            phoneNumber: req.body.phoneNumber,
+            academy_id: req.body.academy_id
+        })
+        await newTeacher.save();
+        res.send(newTeacher);
+    } catch (err){
+        next(err);
+    }
+}
+
+const getTeacher = async (req,res,next) => {
+    try{
+        const id = req.params.id;
+        const teacher = await models.Teacher.findOne({id});
+        res.send(teacher);
+    } catch(err){
+        next(err)
+    }
+}
 module.exports = {
     getTeachers,
+    createTeacher,
+    getTeacher,
 };
--- src/routes/memberRouter.js
+++ src/routes/memberRouter.js
@@ -3,5 +3,8 @@ const router = express.Router()
 const {memberController} = require('../controllers')
 
 router.get("/teachers", memberController.getTeachers)
+router.post("/teachers", memberController.createTeacher)
+router.get("/teachers/:id", memberController.getTeacher)
+
 
 module.exports = router
