5bb3d3080f848d9585c5bfaaa5c021e0354ad2a0
[mochangil] <ahckddlf1234@gmail.com>
2023-05-03 18:16:55 +0900

    Feat : Student CRUD

--- src/controllers/memberController.js
+++ src/controllers/memberController.js
@@ -33,9 +33,55 @@ const createTeacher = async (req,res,next) =>{
 
 const getTeacher = async (req,res,next) => {
     try{
-        const id = req.params.id;
-        const teacher = await models.Teacher.findOne({id});
+        const myId = req.params.id;
+        const teacher = await models.Teacher.findOne({where:{id:myId}});
         res.send(teacher);
+    } catch(err){
+        next(err)
+    }
+}
+
+const getStudents = async (req,res,next) =>{
+    try{
+        const student = await models.Student.findAll();
+        res.send(student);
+    }
+    catch (err){
+        next(err);
+    }
+};
+
+const createStudent = async (req,res,next) =>{
+    try{
+        console.log("connected")
+        cnt = await models.Student.count();
+        if (cnt!=0) {
+            cnt = 1
+        }
+        else{
+            cnt = await models.Student.max('id');
+        }
+        const newStudent = models.Student.build({name: req.params.name});
+        newStudent.set({
+            id: cnt+1,
+            name: req.body.name,
+            password: req.body.password,
+            age: req.body.age,
+            phoneNumber: req.body.phoneNumber,
+            academy_id: req.body.academy_id,
+        })
+        await newStudent.save();
+        res.send(newStudent);
+    } catch (err){
+        next(err);
+    }
+}
+
+const getStudent = async (req,res,next) => {
+    try{
+        const myId = req.params.id;
+        const student = await models.Student.findOne({where:{id:myId}});
+        res.send(student);
     } catch(err){
         next(err)
     }
@@ -44,4 +90,7 @@ module.exports = {
     getTeachers,
     createTeacher,
     getTeacher,
+    getStudent,
+    createStudent,
+    getStudents
 };
