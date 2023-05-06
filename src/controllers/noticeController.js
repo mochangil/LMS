b625d44c1507a5c3b1a14965b375fc37952ffb32
[mochangil] <ahckddlf1234@gmail.com>
2023-05-06 10:07:14 +0900

    Feat : Notice, Lecture CRUD

---
+++ src/controllers/noticeController.js
@@ -0,0 +1,76 @@
+const models = require('../database/models');
+
+const getNotices = async (req,res,next) =>{
+    try{
+        console.log("wow")
+        const notices = await models.Notice.findAll({
+             attributes: ['id','context','endDate','classroom_id','createdAt','updatedAt']
+        });
+        res.send(notices);
+    } catch(err){
+        next(err)
+    }
+
+};
+
+const getNotice = async (req,res,next) => {
+    try{
+        const myId = req.params.id;
+        console.log(myId);
+        const myName = req.params.name;
+        const Notice = await models.Notice.findOne({
+            attributes: ['id','context','endDate','classroom_id','createdAt','updatedAt'],
+            where: {
+                id : myId
+            } 
+        });
+        res.send(Notice);
+    }catch(err){
+        next(err)
+    }
+}
+
+const createNotice = async (req,res,next) => {
+    try{
+        console.log("connected")
+        const counts = await models.Notice.max('id');
+        const newNotice = models.Notice.build({context: req.body.context});
+        newNotice.set({
+            id: counts+1,
+            context: req.body.context,
+            endDate: req.body.endDate,
+            classroom_id: req.body.classroom_id,
+            createdAt: req.body.createdAt,
+            updatedAt: req.body.updatedAt
+        });
+        await newNotice.save();
+        res.send("ok");
+    }catch(err){
+        next(err);
+    }
+}
+
+const deleteNotice = async (req,res,next) => {
+    try{
+        const myId = req.body.id;
+        const myName = req.body.name;
+        console.log(myId)
+        // const name = req.params.name;
+        await models.Notice.destroy({
+            where: {
+                id: myId
+            }
+        });
+        res.send("delete ok")
+        
+    }catch(err){
+        next(err)
+    }
+}
+
+module.exports = {
+    getNotices,
+    getNotice,
+    createNotice,
+    deleteNotice,
+};
--- src/routes/noticeRouter.js
+++ src/routes/noticeRouter.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const {noticeController} = require('../controllers')
+const noticeController = require('../controllers/noticeController')
 
 
-// router.get("/", noticeController.getNotice)
-// router.post("/", noticeController.createNotice)
-// router.get("/:id", noticeController.getNotice)
-// router.post("/delete", noticeController.deleteNotice)
+router.get("/", noticeController.getNotices)
+router.post("/", noticeController.createNotice)
+router.get("/:id", noticeController.getNotice)
+router.post("/delete", noticeController.deleteNotice)
 
 module.exports = router
