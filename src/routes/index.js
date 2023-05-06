ed1d2197cbc8541e9337ba0b9e95b5ac52f80e25
[mochangil] <ahckddlf1234@gmail.com>
2023-05-06 09:50:46 +0900

    Feat : Lecture CRUD

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,9 @@ const router = express.Router();
 const memberRouter = require('./memberRouter')
 const academyRouter = require('./academy/academyRouter')
 const classroomRouter = require('./class/classroomRouter')
+const lectureRouter = require('./lectureRouter')
+const noticeRouter = require('./noticeRouter')
+
 // GET / 라우터
 // router.get('/', (req, res) => {
 //     res.sendFile('home.html', { root: path.join(__dirname, '../public/html') })
@@ -12,5 +15,6 @@ const classroomRouter = require('./class/classroomRouter')
 router.use('/members',memberRouter)
 router.use('/academies', academyRouter)
 router.use("/classrooms", classroomRouter);
-
+router.use("/notices", noticeRouter);
+router.use("/lectures", lectureRouter);
 module.exports = router;
