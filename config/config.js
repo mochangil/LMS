32fe8ed595d68e4806e524fae1bc7deed97280f3
[mochangil] <ahckddlf1234@gmail.com>
2023-05-16 16:21:25 +0900

    Feat: connect with AWS RDS

--- config/config.js
+++ config/config.js
@@ -3,28 +3,28 @@ dotenv.config();
 const env = process.env;
 
 const development = {
-    username: env.DB_USER,
-    password: env.DB_PASSWORD,
+    username: env.DB_USER_AWS,
+    password: env.DB_PASSWORD_AWS,
     database: env.DB_NAME,
-    host: env.DB_HOST,
+    host: env.DB_HOST_AWS,
     dialect: "mysql",
     port: 3306
 };
 
 const production = {
-    username: env.DB_USER,
-    password: env.DB_PASSWORD,
+    username: env.DB_USER_AWS,
+    password: env.DB_PASSWORD_AWS,
     database: env.DB_NAME,
-    host: env.DB_HOST,
+    host: env.DB_HOST_AWS,
     dialect: "mysql",
     port: 3306
 };
 
 const test = {
-    username: env.DB_USER,
-    password: env.DB_PASSWORD,
+    username: env.DB_USER_AWS,
+    password: env.DB_PASSWORD_AWS,
     database: env.DB_NAME,
-    host: env.DB_HOST,
+    host: env.DB_HOST_AWS,
     dialect: "mysql",
     port: 3306
 };
